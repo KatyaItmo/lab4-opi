c2570e42febc66725e56d332bbc6a2c642ca6858
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-09-08 14:35:31 +0300

    done insecure connection

--- backend/src/main/java/org/example/rest/Version.java
+++ backend/src/main/java/org/example/rest/Version.java
@@ -12,6 +12,6 @@ public class Version {
 
     @GET
     public Response getVersion() {
-        return Response.ok("{\"message\": \"4.1.0\"}").build();
+        return Response.ok("{\"message\": \"no-version\"}").build();
     }
 }
