4e1a6300a31737acd0a371e5c87f5065fa1f3820
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-09-14 19:20:13 +0300

    2.3 - unsecure connection

--- backend/src/main/java/org/example/rest/Version.java
+++ backend/src/main/java/org/example/rest/Version.java
@@ -12,6 +12,6 @@ public class Version {
 
     @GET
     public Response getVersion() {
-        return Response.ok("{\"message\": \"5.3.0\"}").build();
+        return Response.ok("{\"message\": \"5.4.0\"}").build();
     }
 }
